7a80a3955e38153aa9ca6364b0f6b336b7c662b3
thearjun <mailarjunadhikari@gmail.com>
2020-05-02 14:52:09 +0545

    Use Material Table for selection

--- frontend/src/components/SelectClient.js
+++ frontend/src/components/SelectClient.js
@@ -1,56 +1,43 @@
-import React from "react";
-import Slider from "react-slick";
+import React, { useState } from "react";
+import MaterialTable from "material-table";
+import Slider from "./Slider";
 
-import SendCommand from "./SendCommand";
+const ActionsOnSelectedRows = (props) => {
+  const [selectedClients, setSelectedClients] = useState([]);
 
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
+  const client_data = [];
+  props.clients.map((item) => {
+    client_data.push({ ...item });
+  });
+
   return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "#00d1b2" }}
-      onClick={onClick}
-    />
-  );
-}
-
-function SamplePrevArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "#00d1b2" }}
-      onClick={onClick}
-    />
-  );
-}
-
-// Settings for Slider
-const settings = {
-  dots: true,
-  infinite: false,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: true,
-  accessibility: true,
-  swipeToSlide: true,
-  centerMode: true,
-  centerPadding: "0px",
-  nextArrow: <SampleNextArrow />,
-  prevArrow: <SamplePrevArrow />,
-};
-
-const selectClient = (props) => {
-  return (
-    <div className="container">
-      <Slider {...settings}>
-        <div>
-          <SendCommand sessionID={props.selected_clients[0]} />
-        </div>
-      </Slider>
+    <div>
+    <Slider selected_clients={selectedClients}/>
+      <MaterialTable
+        title="Connected Clients"
+        columns={[
+          { title: "Name", field: "client_ID" },
+          { title: "Session ID", field: "client_Session_ID" },
+          { title: "Connection Time", field: "password" },
+          { title: "IP Address", field: "client_IP" },
+        ]}
+        data={client_data}
+        options={{
+          selection: true,
+        }}
+        actions={[
+          {
+            tooltip: "Command all selected clients",
+            icon: "code",
+            onClick: (evt, data) => {
+              setSelectedClients(data);
+              console.log(selectedClients);
+            },
+          },
+        ]}
+      />
     </div>
   );
 };
 
-export default selectClient;
+export default ActionsOnSelectedRows;
