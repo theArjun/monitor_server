830f9554c346560f6fae0ee84037eb9d7773049b
thearjun <mailarjunadhikari@gmail.com>
2020-05-02 12:49:23 +0545

    Handle Selected Clients

--- frontend/src/components/SelectClient.js
+++ frontend/src/components/SelectClient.js
@@ -4,48 +4,49 @@ import Slider from "react-slick";
 import SendCommand from "./SendCommand";
 
 function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "#00d1b2" }}
-        onClick={onClick}
-      />
-    );
-  }
-  
-  function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "#00d1b2" }}
-        onClick={onClick}
-      />
-    );
-  }
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "#00d1b2" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "#00d1b2" }}
+      onClick={onClick}
+    />
+  );
+}
+
+// Settings for Slider
+const settings = {
+  dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  accessibility: true,
+  swipeToSlide: true,
+  centerMode: true,
+  centerPadding: "0px",
+  nextArrow: <SampleNextArrow />,
+  prevArrow: <SamplePrevArrow />,
+};
 
 const selectClient = (props) => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    accessibility: true,
-    swipeToSlide: true,
-    centerMode:true,
-    centerPadding : "0px",
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-  };
-
   return (
     <div className="container">
       <Slider {...settings}>
         <div>
-          <SendCommand />
+          <SendCommand sessionID={props.selected_clients[0]} />
         </div>
       </Slider>
     </div>
