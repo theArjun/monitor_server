52e3a143405776cfaf5908a5ea190a2e3cd3aa36
thearjun <mailarjunadhikari@gmail.com>
2020-05-04 22:53:32 +0545

    Add error and status code display and states

--- frontend/src/components/SendCommand.js
+++ frontend/src/components/SendCommand.js
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
+import "./SendCommand.css";
 const ENDPOINT = "http://127.0.0.1:5000/command";
 const socket = socketIOClient(ENDPOINT); // socket declaration shall be at top.
 
 const SendCommand = (props) => {
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("date");
-  const [output, setOutput] = useState("No commands executed yet !");
+  const [output, setOutput] = useState("");
+  const [error, setError] = useState("");
+  const [returnCode, setReturnCode] = useState("");
 
   props.socket.on("output_from_client_to_web", (commandOutput) => {
-    setOutput(commandOutput);
+    const output = JSON.parse(commandOutput);
+
+    // Show output to respective clients.
+    if (output.session_ID === props.client_Session_ID) {
+      console.log(output.session_ID);
+      setOutput(output.stdout);
+      setError(output.stderr);
+      setReturnCode(output.return_code);
+    }
   });
 
   let commandToSend = {
@@ -33,7 +44,7 @@ const SendCommand = (props) => {
   return (
     <div className="tile is-parent">
       <article className="tile is-child notification is-bordered has-text-centered">
-      
+        <div className="is-family-monospace disp rcode">{returnCode}</div>
         <p className="title">{props.client_ID}</p>
         <p className="subtitle">Session ID : {props.client_Session_ID}</p>
         <br />
@@ -53,8 +64,12 @@ const SendCommand = (props) => {
           </div>
         </form>
         <br />
-        <p className="subtitle">Output : </p>
-        <p className="subtitle is-family-monospace">{output}</p>
+        {output.length > 0 ? (
+          <div className="is-family-monospace disp output">{output}</div>
+        ) : null}
+        {error.length > 0 ? (
+          <div className="is-family-monospace disp error">{error}</div>
+        ) : null}
       </article>
     </div>
   );
