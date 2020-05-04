fbbe400113a446ed7ad4d70c015d626fe8c09905
thearjun <mailarjunadhikari@gmail.com>
2020-05-05 04:47:47 +0545

    Updates

---
+++ precache-manifest.1c3205d8d8c970f2d40a107eb860f582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a897079b891fc6eefc8b4cfd05b9764",
+    "url": "/monitor_server/index.html"
+  },
+  {
+    "revision": "846b96d2440e6a36a157",
+    "url": "/monitor_server/static/css/main.c84327e5.chunk.css"
+  },
+  {
+    "revision": "d6ff9a7e786dc23adb3d",
+    "url": "/monitor_server/static/js/2.2bd5ec92.chunk.js"
+  },
+  {
+    "revision": "3c45ea21ae39fd33204d7de3baeeaaa3",
+    "url": "/monitor_server/static/js/2.2bd5ec92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "846b96d2440e6a36a157",
+    "url": "/monitor_server/static/js/main.e9435c31.chunk.js"
+  },
+  {
+    "revision": "7188a0da61df00277e2a",
+    "url": "/monitor_server/static/js/runtime-main.ba851e8e.js"
+  }
+]);
